0433386957ec5343affc659514662b090197d786
Marcos Dalmasso <marcosadriandalmasso@hotmail.com>
2016-02-09 21:07:44 -0300

    c1e3

--- gemStore/js/gemStore.js
+++ gemStore/js/gemStore.js
@@ -1,10 +1,15 @@
 (function() {
-    var gem = { name: 'Azurite', price: 2.95 };
+    var gem = { name: 'Azurite', price: 2.95, canPurchase: false, soldOut: false };
     
     angular
         .module('gemStore', [])
-        .controller('StoreController', function() {
+        .controller('StoreController', function($log) {
             this.product = gem;
+            this.purchase = purchase;
+
+            function purchase() {
+            	$log.warn('StoreController.purchase NOT_IMPLEMENTED');
+            }
         });
     
 })()
