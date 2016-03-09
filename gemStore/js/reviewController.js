918afd1ab1f60218409b68dec4aac89375b7b7c7
Marcos Dalmasso <marcosadriandalmasso@hotmail.com>
2016-03-08 23:45:09 -0300

    c3e4

---
+++ gemStore/js/reviewController.js
@@ -0,0 +1,27 @@
+(function() {
+
+    'use strict';
+    
+    angular
+        .module('gemStore')
+        .controller('reviewController', ReviewController);
+        
+    function ReviewController() {
+
+        var vm = this;
+        vm.addReview = addReview;
+        reset();
+        
+        function reset() {
+
+            vm.review = {};
+
+        };
+        
+        function addReview(product) {
+            product.reviews.push(vm.review);
+            reset();
+        }
+    }
+
+})();
